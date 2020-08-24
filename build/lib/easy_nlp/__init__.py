6abf5f884ff4d22e61d533ff34ceaf4d6516a2bc
RajeshNathani <rajeshnathani055@gmail.com>
2020-08-24 10:15:58 +0530

    first commit

---
+++ build/lib/easy_nlp/__init__.py
@@ -0,0 +1,38 @@
+import spacy
+from spacy import displacy
+nlp = spacy.load("en_core_web_sm")
+
+class extract:
+    def POS(str):
+        parts = nlp(str)
+        sent = dict()
+        for token in parts:
+            sent[token.text] = token.pos_
+        return(sent)
+    def base(str):
+        parts = nlp(str)
+        bas = dict()
+        for token in parts:
+            bas[token.text] = token.lemma_
+        return(bas)
+    def token(str):
+        doc = nlp(str)
+        return [token.text for token in doc]
+
+    def names(str):
+        doc = nlp(str)
+        return [(ent.text , ent.label_) for ent in doc.ents]
+
+class dependency:
+    def dep_show(str):
+        doc = nlp(str)
+        return displacy.serve(doc , style='ent')
+
+class sentence:
+    def split(str):
+        text = nlp(str)
+        sentences =[]
+        for sent in text.sents:
+            sentences.append(sent)
+        return sentences
+ 
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='easy nlp',
+version = '0.1.1',
+description = "NLP made easy.",
+author = "Rajesh Nathani" ,
+author_email = "rajeshnathani055@gmail.com",
+license = "MIT" ,
+install_requires =['spacy==2.2'],
+packages = ['easy_nlp'] , zip_safe = False
+)
